17412fad1962f2d1bd2efdc33953140bec52c669
Sergio <predok512k@gmail.com>
2018-05-04 16:45:42 +0300

    refactor

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const cors = require('cors');
-const puppeteer = require('puppeteer');
+// const cors = require('cors');
 const helmet = require('helmet');
+const FormData = require('form-data');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 const http = require('http');
+const https = require('https');
 const parseString = require('xml2js').parseString;
 
 const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 8080;
 
-const tomorrow = date => {
+let form = new FormData();
+
+let tomorrow = date => {
   let dd = date.getDate() + 1; //We need tomorrow
   let mm = date.getMonth() + 1; //Month starts at 0!
   let yyyy = date.getFullYear();
@@ -21,188 +26,137 @@ const tomorrow = date => {
   return dd + '/' + mm + '/' + yyyy;
 };
 
-let exchangeRate;
+const exchangeRate = 86.4616;
 
-function getRate(callback) {
-  http.get(
-    `http://www.cbr.ru/scripts/XML_daily.asp?date_req=${tomorrow(new Date())}`,
-    res => {
-      let data = '';
-      res.on('data', chunk => {
-        data += chunk;
-      });
-      res
-        .on('end', () => {
-          parseString(data, (err, res2) => {
-            let rate = parseFloat(
-              res2.ValCurs.Valute[2].Value[0].replace(',', '.')
-            );
-            // console.log(a);
-            callback(rate);
-          });
-        })
-        .on('error', err => {
-          throw err;
-        });
-    }
-  );
+const rusPrice = price => {
+  // if (price typeof !== 'string')
+  const result = Math.round(price.replace('£', '') * exchangeRate / 50) * 50;
+  return result;
+};
+
+function platformTranslate(input) {
+  if (typeof input !== 'string') return 'input must be a string';
+  let output;
+  switch (input) {
+  case 'XB2':
+    output = 'Xbox 360';
+    break;
+  case 'XB3':
+    output = 'Xbox One';
+    break;
+  case 'WIU':
+    output = 'Wii U';
+    break;
+  case 'WII':
+    output = 'Wii';
+    break;
+  case 'NSW':
+    output = 'NSwitch';
+    break;
+  default:
+    output = input;
+  }
+  return output;
 }
 
-getRate(rate => (exchangeRate = rate));
+// let exchangeRate;
 
-setInterval(function getRate(rate) {
-  return (exchangeRate = rate);
-}, 86400000);
+// function getRate(callback) {
+//   http.get(
+//     `http://www.cbr.ru/scripts/XML_daily.asp?date_req=${tomorrow(new Date())}`,
+//     res => {
+//       let data = '';
+//       res.on('data', chunk => {
+//         data += chunk;
+//       });
+//       res
+//         .on('end', () => {
+//           parseString(data, (err, res2) => {
+//             let rate = parseFloat(
+//               res2.ValCurs.Valute[2].Value[0].replace(',', '.')
+//             );
+//             // console.log(a);
+//             callback(rate);
+//           });
+//         })
+//         .on('error', err => {
+//           throw err;
+//         });
+//     }
+//   );
+// }
 
-/*
-const ymStart = 'https://market.yandex.ru/product/';
-const ymEnd = '/offers?local-offers-first=1&how=aprice';
-*/
+// getRate(rate => {
+//   console.log(`Tomorrow(${tomorrow(new Date())}) 1 GBP for ${rate} roubles`);
+//   return (exchangeRate = rate);
+// });
+
+// setInterval(function getRate(rate) {
+//   return (exchangeRate = rate);
+// }, 86400000);
+
 const app = express();
 
 app.use(helmet());
-app.use(cors());
-// app.disable('x-powered-by');
+// app.use(cors());
 
-//Работаем с yandex.market
-/*
-app.get('/ym/:id', (req, res) => {
-  res.send(req.params.id);
-});
-*/
+app.get('/gcu/:id', (req, res) => {
+  console.log(req.params.id);
+  form.append('TechKeyword', '');
+  form.append('SoftwareKeyword', `${req.params.id}`);
+  form.append('button', 'Search');
+  form.append('FirstName', '');
+  form.append('LastName', '');
+  form.append('MinimumBasketValue', 5);
 
-//Работаем с amazon.co.uk
-/*
-app.get('/amazon/:id', (req, res) => {
-  try {
-    (async () => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.setViewport({ width: 1280, height: 800 });
-      await page.goto('https://www.amazon.co.uk');
-      await page.type('#twotabsearchtextbox', req.params.id);
-      await page.click('input.nav-input');
-      await page.waitForSelector('#resultsCol');
-      // await page.screenshot({ path: 'search_in_list.png' });
-      // const firstProductName = await page.$('a.a-link-normal.a-text-normal');
+  (async function() {
+    const response = await fetch(
+      'https://tradein.game.co.uk/?cm_sp=TradeInOnline-_-Portal-_-CheckPrices',
+      {
+        method: 'POST',
+        body: form
+        // headers: {
+        //   'Content-Type': 'application/x-www-form-urlencoded'
+        // }
+      }
+    );
+    const data = await response.text();
+    const $ = await cheerio.load(data);
+    const result = [];
 
-      const cardData = await page
-        .evaluate(() => {
-          const card = document.querySelector('#result_0');
-          const title = card.querySelector('h2.a-text-normal').innerText;
-          const platforms = Array.from(
-            card.querySelectorAll('.a-size-small.s-inline.a-text-normal')
-          );
-          const allPlatforms = platforms.map(el => el.textContent);
-          const prices = Array.from(card.querySelectorAll('.a-color-price'));
-          const allPrices = prices.map(el => el.textContent);
-
-          return { title: title, platform: allPlatforms, price: allPrices };
-        })
-        .catch(error => console.log('TryCatch Error', error));
-
-      console.log('Result is: ', cardData);
-      await browser.close();
-    })();
-  } catch (err) {
-    console.error(err);
-  }
-});
-*/
-
-//Работаем с games.co.uk
-app.get('/gcu/:id', cors(), async (req, res) => {
-  try {
-    (async () => {
-      const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-      const page = await browser.newPage();
-      await page.setViewport({ width: 1280, height: 800 });
-      await page.goto(
-        'https://tradein.game.co.uk/?cm_sp=TradeInOnline-_-Portal-_-CheckPrices'
-      );
-      await page.waitForSelector('#searchTab');
-      await page.click('a[href="#searchSoftware"]');
-      await page.type('#SoftwareKeyword', req.params.id, {
-        delay: Math.floor(Math.random() * 100) + 50
+    $('div.row.row-border').each((i, el) => {
+      result.push({
+        id: 'unknown',
+        title:
+          $(el)
+            .find('div.prod-title')
+            .text() +
+          ' ' +
+          platformTranslate(
+            $(el)
+              .find('#platformImage')
+              .attr('alt')
+          ),
+        price: rusPrice(
+          $(el)
+            .find('span.credit-price-field')
+            .text()
+        ),
+        priceForCash: rusPrice(
+          $(el)
+            .find('span.price-field')
+            .text()
+        ),
+        cover: $(el)
+          .find('div.col-xs-6.col-md-4')
+          .children()
+          .first()
+          .attr('src')
       });
-
-      //(node:26098) UnhandledPromiseRejectionWarning: Error: Protocol error (Runtime.callFunctionOn): Cannot find context with specified id undefined
-      // await page.keyboard.press('Enter', { delay: 50 });
-
-      await page.click('#SoftwareSearch.btn.btn-primary.btn-lg');
-      await page.waitForSelector('.cushion');
-      // await page.screenshot({ path: 'gcu_before.png' });
-
-      await page.addScriptTag({
-        content: `function platformToString(input) {
-        if (typeof input !== 'string') return 'input must be a string';
-        let output;
-        switch (input) {
-        case 'XB2':
-          output = 'Xbox 360';
-          break;
-        case 'XB3':
-          output = 'Xbox One';
-          break;
-        case 'WIU':
-          output = 'Wii U';
-          break;
-        case 'WII':
-          output = 'Wii';
-          break;
-        case 'NSW':
-          output = 'NSwitch';
-          break;
-        default:
-          output = input;
-        }
-        return output;
-      };
-      `
-      });
-      await page.addScriptTag({ content: `let exchangeRate=${exchangeRate}` });
-
-      const responseToReq = await page.evaluate(() => {
-        let result = [];
-        /* eslint-disable */
-        document.querySelectorAll('div.row.row-border').forEach(el => {
-          /* eslint-enable */
-          if (el.hasChildNodes()) {
-            const title = el.querySelector('div.prod-title').textContent;
-            const credit = el.querySelector('span.credit-price-field')
-              .textContent;
-            const cash = el.querySelector('span.price-field').textContent;
-            const platform = el
-              .querySelector('#platformImage')
-              .getAttribute('alt');
-            const image = el
-              .querySelector('div.col-xs-6.col-md-4')
-              .firstElementChild.getAttribute('src');
-            const id = 'unknown';
-            result.push({
-              id: id || 'unknown',
-              /* eslint-disable */
-              title: title + ' ' + platformToString(platform) || 'unknown',
-              /* eslint-enable */
-              price:
-                Math.round(credit.replace('£', '') * exchangeRate / 50) * 50 ||
-                'unknown',
-              priceForCash:
-                Math.round(cash.replace('£', '') * exchangeRate / 50) * 50 ||
-                'unknown',
-              cover: image || 'unknown'
-            });
-          }
-        });
-
-        return result;
-      });
-      await browser.close();
-      res.status(200).json(responseToReq);
-    })();
-  } catch (err) {
-    throw err;
-  }
+    });
+    console.log(result);
+    res.status(200).json(result);
+  })();
 });
 
 app.get('/favicon.ico', (req, res) => res.status(204).end());
@@ -211,4 +165,4 @@ app.use('*', (req, res) => {
   res.status(403).end();
 });
 
-app.listen(PORT, HOST);
+app.listen(PORT, HOST, console.log('Started'));
