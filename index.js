c885a3ab53aff637bd94c25658cc4e7182887628
Pr1ma <predok512k@gmail.com>
2018-05-06 22:43:14 +0300

    save

--- index.js
+++ index.js
@@ -170,7 +170,9 @@ app.get('/gcu/:id', (req, res) => {
               /img\.game\.co\.uk\/assets\/img\/_tradein-img\/icon_grey\.jpg/
             ) === -1
               ? cover
-              : '<i class="material-icons">broken_image</i>',
+              : `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+              <path fill="#000000" d="M8.5,13.5L11,16.5L14.5,12L19,18H5M21,19V5C21,3.89 20.1,3 19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19Z" />
+          </svg>`,
           filters: {
             platform: platform
           }
