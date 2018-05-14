b13fac8967d3cb41e164aad668462a6801421616
Pr1ma <predok512k@gmail.com>
2018-05-15 01:08:12 +0300

    remove dist in package3

--- cache.js
+++ cache.js
@@ -1,25 +1,25 @@
-var cache = require('memory-cache');
+// var cache = require('memory-cache');
 
-cache.put('foo', 'rush b suka');
-console.log(cache.get('id'));
+// cache.put('foo', 'rush b suka');
+// console.log(cache.get('id'));
 
-cache.put('houdini', 'disappear', 2000, function(key, value) {
-  console.log(key + ' did ' + value);
-});
+// cache.put('houdini', 'disappear', 2000, function(key, value) {
+//   console.log(key + ' did ' + value);
+// });
 
-console.log('Houdini will now ' + cache.get('houdini'));
+// console.log('Houdini will now ' + cache.get('houdini'));
 
-setTimeout(function() {
-  console.log('Houdini is ' + cache.get('houdini'));
-}, 4000);
+// setTimeout(function() {
+//   console.log('Houdini is ' + cache.get('houdini'));
+// }, 4000);
 
-var newCache = new cache.Cache();
+// var newCache = new cache.Cache();
 
-newCache.put('foo', 'newbaz');
+// newCache.put('foo', 'newbaz');
 
-setTimeout(function() {
-  console.log('foo in old cache is ' + cache.get('foo'));
-  console.log('foo in new cache is ' + newCache.get('foo'));
-}, 6000);
+// setTimeout(function() {
+//   console.log('foo in old cache is ' + cache.get('foo'));
+//   console.log('foo in new cache is ' + newCache.get('foo'));
+// }, 6000);
 
-console.log(cache.memsize());
+// console.log(cache.memsize());
